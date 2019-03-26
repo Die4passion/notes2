30e397f31f164bc20a1c3f2299cd7b033024fd9b
die4passion <die4passion@qq.com>
2019-03-26 12:58:00 +0800

    add rtd

--- conf.py
+++ conf.py
@@ -10,8 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+
+
+import os
+import sys
+import shlex
+
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -54,9 +58,30 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# This allows sphinx_rtd_theme to work locally
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+html_context = {
+    'on_rtd' : on_rtd
+}
+
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_last_updated_fmt = '%b %d, %Y'
+html_search_language = 'zh'
+
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
